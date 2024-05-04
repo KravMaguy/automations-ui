f91108c4a65f165f64fde3899f3835c1f0bfcdbb
kravmaguy <37171082+KravMaguy@users.noreply.github.com>
2024-05-04 16:21:04 -0400

    fix chrome is not defined

--- plugin/eslint.config.mjs
+++ plugin/eslint.config.mjs
@@ -1,8 +1,13 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: {
+        chrome: true,
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
