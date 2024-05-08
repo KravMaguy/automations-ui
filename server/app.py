8789ea326cba5ff1585ba9a116bf15414afd0f4f
kravmaguy <37171082+KravMaguy@users.noreply.github.com>
2024-05-08 17:29:16 -0400

    get last form value

--- server/app.py
+++ server/app.py
@@ -2,12 +2,20 @@ from flask import Flask, json, request, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/upload', methods=['POST'])
 def upload_data():
-    data = request.json 
+    data = request.json
+    formFieldData = {}
     for item in data:
-        print(item, ' item')
+        field_name, value = item[0], item[1]
+        if field_name not in formFieldData or formFieldData[field_name] != value:
+            formFieldData[field_name] = value
+
+    print(formFieldData)
+
     return jsonify(status="success", message="Data received")
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
