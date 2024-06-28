661fa684f91e9054c0c69080ce7fcbabf0f1a526
kravmaguy <37171082+KravMaguy@users.noreply.github.com>
2024-06-28 13:31:24 -0400

    fix: recieving end does not exist actively recorded tab was closed

--- plugin/background.js
+++ plugin/background.js
@@ -15,12 +15,14 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "startRecording") {
     chrome.storage.local.set({ isRecording: true });
   } else if (request.action === "stopRecording") {
+    console.log("stoppin reco");
     chrome.storage.local.set({ isRecording: false });
   }
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete") {
+    console.log("tabs updated complete");
     // When a tab is updated, restore the recording state from storage
     // chrome.storage.local.get(["isRecording"], function (result) {
     //   if (result.isRecording) {
@@ -31,19 +33,20 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   }
 });
 
-// // Failsafe to stop recording if the recorded tab is closed
-// chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
-//   chrome.storage.local.get(["automations"], function (result) {
-//     const automations = result.automations || [];
-//     const automationIndex = automations.findIndex(
-//       (a) => a.status === "Recording" && a.tabId === tabId
-//     );
-//     if (automationIndex !== -1) {
-//       automations[automationIndex].status = null;
-//       chrome.storage.local.set({ automations });
-//       console.log(7);
-//       chrome.tabs.sendMessage(tabId, { action: "stopRecording" });
-//       chrome.storage.local.set({ isRecording: false });
-//     }
-//   });
-// });
+//stop recording if user closes the tab of the page being recorded
+chrome.tabs.onRemoved.addListener((tabId, removeInfo) => {
+  chrome.storage.local.get(["automations"], function (result) {
+    const automations = result.automations || [];
+    const automationIndex = automations.findIndex(
+      (a) => a.status === "Recording" && a.tabId === tabId
+    );
+    if (automationIndex !== -1) {
+      automations[automationIndex].status = null;
+      chrome.storage.local.set({ automations });
+      console.log(7);
+      console.log("tab being removed is actively being recorded");
+      // chrome.tabs.sendMessage(tabId, { action: "stopRecording" });
+      chrome.storage.local.set({ isRecording: false });
+    }
+  });
+});
